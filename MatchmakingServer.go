8b4e1b79695a8a74782958a0a95d4e6a2114eddc
Parth Mehrotra <parth@mehrotra.me>
2018-08-06 20:08:07 -0400

    matchmaker

---
+++ MatchmakingServer.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+var matchmaker *Matchmaker
+
+func main() {
+	fmt.Println("Matchmaker started")
+	matchmaker = NewMatchmaker(3)
+
+	http.HandleFunc("/ws", wsHandler)
+
+	panic(http.ListenAndServe(":8000", nil))
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := websocket.Upgrade(w, r, w.Header(), 1024, 1024)
+	if err != nil {
+		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
+	}
+
+	matchmaker.PlayerJoined(conn)
+}
---
+++ matchmaker.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/websocket"
+
+	"github.com/go-redis/redis"
+)
+
+type Matchmaker struct {
+	StatusChannels     []chan string
+	GameserverChannels []chan string
+	RedisClient        *redis.Client
+	CurrentClients     int
+	TargetClients      int
+}
+
+func NewMatchmaker(target int) *Matchmaker {
+	var client *redis.Client
+	for {
+		fmt.Println("Attempting to connect to redis")
+		client = redis.NewClient(&redis.Options{
+			Addr:     "redis:6379",
+			Password: "",
+			DB:       0,
+		})
+		_, err := client.Ping().Result()
+		if err != nil {
+			fmt.Println("Matchmaker could not connect to redis")
+			fmt.Println(err)
+		} else {
+			break
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+	fmt.Println("Connected to redis")
+	return &Matchmaker{nil, nil, client, 0, target}
+}
+
+func (m *Matchmaker) getIdleGameserver() string {
+	c := m.RedisClient
+
+	keys, _ := c.Keys("*").Result()
+
+	for _, key := range keys {
+		status, _ := c.Get(key).Result()
+		if status == "idle" {
+			err := c.Set(key, strconv.Itoa(m.CurrentClients), 0).Err()
+
+			if err == nil {
+				// TODO before returning you need to check to make sure gameserver is on the same page
+				return key
+			} else {
+				fmt.Println("Could not find idle gameserver")
+				fmt.Println(err)
+			}
+		}
+	}
+
+	return ""
+}
+
+func (m *Matchmaker) PlayerJoined(conn *websocket.Conn) {
+	m.CurrentClients++
+
+	status := make(chan string)
+	m.StatusChannels = append(m.StatusChannels, status)
+
+	gameserver := make(chan string)
+	m.GameserverChannels = append(m.GameserverChannels, gameserver)
+
+	go m.syncMatchmaker(conn, status, gameserver)
+
+	for _, statusChannel := range m.StatusChannels {
+		statusChannel <- "nop"
+	}
+
+	if m.CurrentClients == m.TargetClients {
+
+		selectedPort := m.getIdleGameserver()
+
+		for _, gameChannel := range m.GameserverChannels {
+			gameChannel <- selectedPort
+		}
+
+		m.CurrentClients = 0
+		//TODO  cleanup here
+	}
+}
+
+func (m *Matchmaker) syncMatchmaker(conn *websocket.Conn, status chan string, gameserver chan string) {
+	for {
+		select {
+		case <-status:
+			if err := conn.WriteJSON(map[string]map[string]int{
+				"Status": {
+					"CurrentClients": m.CurrentClients,
+					"TargetClients":  m.TargetClients,
+				},
+			}); err != nil {
+				fmt.Println(err)
+			}
+		case gs := <-gameserver:
+			if err := conn.WriteJSON(map[string]string{
+				"Port": gs,
+			}); err != nil {
+				fmt.Println(err)
+			}
+			conn.Close()
+		}
+	}
+}
